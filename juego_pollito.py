c246429b0f448dcf08790d36533c7e839b10a190
Marianasofid <marianadelgado@colegioguanenta.edu.co>
2025-04-24 22:27:19 -0500

    actualizacion

--- juego_pollito.py
+++ juego_pollito.py
@@ -7,43 +7,17 @@ blanco = (255,255,255)
 negro = (0,0,0)
 pygame.init()
 
+pygame.init()
+
 ventana = pygame.display.set_mode((800,600))
 pygame.display.set_caption("Salva al pollito")
 
-clock = pygame.time.Clock()
-
-while 1:
-    clock.tick(50)
-
+while True: 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT: 
+            pygame.quit()
             sys.exit()
 
     ventana.fill(blanco)
 
-    pygame.draw.rect(ventana, blanco, ((50,50), (400,400)), 1)
-
-
-
-ventana.fill(blanco)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-pygame.display.flip()
+    pygame.display.flip()
